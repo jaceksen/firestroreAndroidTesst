dbacd1aab6a8b2d5cf59fb0649d7252709345436
jaceksen <jaceksen@gmail.com>
2017-10-30 17:06:13 +0100

    Dodana data i status

--- app/src/main/java/com/example/jsen/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/jsen/firebasetest/MainActivity.java
@@ -11,12 +11,15 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String ZGLOSZENIE_KLUCZ = "opis_zgloszenia";
+    public static final String DATA_KLUCZ = "data_wpisu";
+    public static final String STATUS_KLUCZ = "status";
 //    private DocumentReference mDocRef= FirebaseFirestore.getInstance().document("usterki/usterka");
     private CollectionReference mColRef = FirebaseFirestore.getInstance().collection("usterki");
 
@@ -29,10 +32,14 @@ public class MainActivity extends AppCompatActivity {
     public void saveZgloszenie(View view) {
         EditText tekstZgloszenia = (EditText) findViewById(R.id.etZgloszenie);
         String tekstZgString = tekstZgloszenia.getText().toString();
+        Date dataWpisu = new Date();
+        String statusZgloszenia = "zgłoszone";
 
         if(tekstZgString.isEmpty()){ return; }
         Map<String, Object> dataToSave = new HashMap<String, Object>();
         dataToSave.put(ZGLOSZENIE_KLUCZ,tekstZgString);
+        dataToSave.put(DATA_KLUCZ,dataWpisu);
+        dataToSave.put(STATUS_KLUCZ,statusZgloszenia);
         //mDocRef.set(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
         mColRef.add(dataToSave).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
             @Override
