f74c5717b89cf872a41009efc9a2a9c002d85372
jaceksen <jaceksen@gmail.com>
2017-11-03 17:16:56 +0100

    OK - dodawanie wpisów

--- app/src/main/java/com/example/jsen/firebasetest/MainActivity.java
+++ app/src/main/java/com/example/jsen/firebasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.jsen.firebasetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -48,4 +50,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
